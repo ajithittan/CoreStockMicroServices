22735f8ea357d16ad94723c06c6cef951009ebb3
ajithittan <ajithittan@gmail.com>
2022-11-13 09:47:03 -0500

    ok
    ok

--- routes/stockpredictions.js
+++ routes/stockpredictions.js
@@ -1,13 +1,14 @@
 const URLConfig = require("../config/url.config.js");
 const urlconf = new URLConfig()
 const URL_HOST = urlconf.HOST2
+const URL_PREDICTIONS = urlconf.STK_PREDICTIONS
 
 module.exports = (app) => {
-  app.get('/api/predictions/allstkmodels', async (req, res) => {
+  app.get('/api/predictionmodels/:stksym', async (req, res) => {
     const fetch = require("node-fetch");
     let response
     try{
-      await fetch(URL_HOST + 'StkPrediction/PredictionModels')
+      await fetch(URL_PREDICTIONS + 'predictions/models/' + req.params.stksym)
       .then(res => res.json())
       .then(json => {response=json});
     }
@@ -20,7 +21,7 @@ module.exports = (app) => {
     const fetch = require("node-fetch");
     let response
     try{
-      await fetch(URL_HOST + 'StkPrediction/Prediction/' + req.params.model + '/' + req.params.stksym)
+      await fetch(URL_PREDICTIONS + 'predictions/predict/' + req.params.stksym  + '/' + req.params.model)
       .then(res => res.json())
       .then(json => {response=json});
     }
@@ -42,4 +43,36 @@ module.exports = (app) => {
     }
     return res.status(200).send(response);
   });  
-}
+  app.post('/api/genpredictions/:stksym/:model', async (req, res) => {
+    const fetch = require("node-fetch");
+    let response
+    try{
+      console.log("req",req.body)
+      await fetch(URL_PREDICTIONS + 'predictions/' + req.params.stksym + "/" + req.params.model, 
+                                  {method:'post', body:JSON.stringify(req.body), 
+                                  headers: { 'Content-Type': 'application/json' }})
+      .then(res => res.json())
+      .then(json => {response=json});
+    }
+    catch (err){
+      console.log(err)
+    }
+    return res.status(200).send(response);
+  });
+  app.post('/api/savemodel/:stksym/:model', async (req, res) => {
+    const fetch = require("node-fetch");
+    let response
+    try{
+      console.log("req",req.body)
+      await fetch(URL_PREDICTIONS + 'predictions/save/' + req.params.stksym + "/" + req.params.model, 
+                                  {method:'post', body:JSON.stringify(req.body), 
+                                  headers: { 'Content-Type': 'application/json' }})
+      .then(res => res.json())
+      .then(json => {response=json});
+    }
+    catch (err){
+      console.log(err)
+    }
+    return res.status(200).send(response);
+  });
+  }
